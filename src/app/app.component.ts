807ebc56bffcd844df3e944a44041488d7360f9b
Samantha Tartamella <Samantha.Tartamella@veteransunited.com>
2024-10-04 18:26:55 -0500

    feat: ui header, wip menu button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MainComponent,
-    ThemeToggleComponent,
-    ThemeToggleComponent,
-  ],
+    MainComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatButtonToggleModule],
+  imports: [
+    MatButtonToggleModule,
+    MatCardModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+  imagePath: string = '/assets/whiteshrimp.png';
+}
