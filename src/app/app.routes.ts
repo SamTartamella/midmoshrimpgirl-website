55c09d7966588e651184c6805a0ae957091a08d5
Samantha Tartamella <Samantha.Tartamella@veteransunited.com>
2024-10-09 13:30:18 -0500

    refactor: changed routing per comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { RedCherryProductComponent } from './components/red-cherry-product/red-cherry-product.component';
-import { MainComponent } from './components/main/main.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: 'red-cherry', component: RedCherryProductComponent },
+  //   { path: 'red-cherry', component: RedCherryProductComponent },
   { path: '', component: HomeComponent },
+  { path: 'products/:item', component: RedCherryProductComponent }, //Will make product component. Template for product, info filled in from DB eventually.
 ];
