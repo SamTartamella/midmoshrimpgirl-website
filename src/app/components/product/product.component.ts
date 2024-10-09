b842c9397bb0ef3505f58a8b5ff3024647625860
Samantha Tartamella <Samantha.Tartamella@veteransunited.com>
2024-10-09 14:32:05 -0500

    feat: info in product page wip

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  productName: string = 'Test';
+  productPrice: number = 0.0;
+  tempImagePath: string = '/assets/whiteshrimp.png';
+
   getProduct() {}
 }
---
+++ src/app/services/getProductInfo/get-product-info.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GetProductInfoService } from './get-product-info.service';
+
+describe('GetProductInfoService', () => {
+  let service: GetProductInfoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GetProductInfoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
