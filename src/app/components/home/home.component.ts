989e7d7428d2f36e3fdc3aaaf59f06a2f767a534
Samantha Tartamella <samantha.tartamella@veteransunited.com>
2024-12-30 15:41:28 -0600

    feat: updated ui on homepage to be pretty

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule, MatButtonModule, RouterOutlet, RouterLink],
+  imports: [MatGridListModule, MatButtonModule, RouterOutlet, RouterLink, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
